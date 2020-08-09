464863a8ada657e25c5355f59785a08563220679
lucaskmiyazaki <lucaskmiyazaki@hotmail.com>
2020-08-09 10:44:04 -0300

    test: improve video test

--- tests/video_test.py
+++ tests/video_test.py
@@ -3,6 +3,9 @@ import json
 import numpy as np
 import requests
 import sys
+from zipfile import ZipFile
+import io
+import os
 
 frame = cv2.imread("../temp/ref.jpeg")
 frame = cv2.resize(frame, None, fx=0.4, fy=0.4)
@@ -23,20 +26,43 @@ else:
 
 video = cv2.VideoCapture("../temp/pao.mp4")
 ok = True
-while ok:
-    ok, frame = video.read()
-    frame = cv2.resize(frame, None, fx=0.2, fy=0.2)
-    frame = frame.tolist()
-    hashtable = {"img": frame, "mean": mean, "std": std}
-    data = json.dumps(hashtable)
-    print(sys.getsizeof(data))
-    try:
-        res = requests.post('http://aws-test.eba-gajbic4g.sa-east-1.elasticbeanstalk.com/api/segmentation', json = data)
-    except:
-        res = requests.post('http://localhost:5000/api/segmentation', json = data)
-    if res.ok:
-        res = res.json()
-        print(res)
-    else:
-        print(res)
+count = 0
+max_frame = 10
+class_number = 0
+class_name = "pao"
+path = "../temp/"
+#zipObj = ZipFile(path+'sample.zip', 'w')
+with ZipFile(path+'sample.zip', mode="w") as zf:
+    while ok or count > max_frame:
+        ok, frame = video.read()
+        frame = cv2.resize(frame, None, fx=0.2, fy=0.2)
+        img = frame.tolist()
+        hashtable = {"img": img, "mean": mean, "std": std}
+        data = json.dumps(hashtable)
+        print(sys.getsizeof(data))
+        try:
+            res = requests.post('http://aws-test.eba-gajbic4g.sa-east-1.elasticbeanstalk.com/api/segmentation', json = data)
+        except:
+            res = requests.post('http://localhost:5000/api/segmentation', json = data)
+        if res.ok:
+            res = res.json()
+            print(res)
+            cv2.imwrite("%s%d.jpg" %(path+class_name, count), frame)     
+            #buf = io.BytesIO()
+            #cv2.imwrite(buf, frame)
+            #zf.writestr("%s%d.jpg" %(path+class_name, count), buf.getvalue())
 
+            #buf = io.BytesIO()
+            box = (class_number, res['x'], res['y'], res['w'], res['h'])
+            f = open("%s%d.txt" %(path+class_name, count), 'w')
+            #f = open(buf, 'w')
+            f.write("%d %f %f %f %f" %box)
+            f.close()
+            #zf.writestr("%s%d.txt" %(path+class_name, count), buf.getvalue())
+            zf.write("%s%d.jpg" %(path+class_name, count))
+            zf.write("%s%d.txt" %(path+class_name, count))
+            os.remove("%s%d.jpg" %(path+class_name, count))
+            os.remove("%s%d.txt" %(path+class_name, count))
+            count += 1
+        else:
+            print(res)
